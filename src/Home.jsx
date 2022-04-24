6a485fdb67a8059819b393c9ef190848adff8151
Aniket <aniketsaste96@gmail.com>
2022-04-24 14:25:23 +0530

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+
 const Home = () => {
+  const [change, setChange] = useState(0);
   const dispatch = useDispatch();
 
   const addBtn = () => {
@@ -16,15 +18,16 @@ const Home = () => {
   const addBtn25 = () => {
     dispatch({
       type: "incrementByValue",
-      payload: 25,
+      payload: Number(change),
     });
   };
-
+  console.log(typeof change);
   return (
     <div>
       <button onClick={addBtn}>Incremenet</button>
       <button onClick={subBtn}>Decremenet</button>
-      <button onClick={addBtn25}>Incremenet by 25</button>
+      <input onChange={(e) => setChange(e.target.value)} />
+      <button onClick={addBtn25}>+25</button>
     </div>
   );
 };
